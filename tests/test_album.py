624ecce2670fdd0ce2ab16986f6f018129a8245b
zearkiatos <caprilespe@outlook.com>
2023-02-18 20:52:59 -0300

    Create a class to agroup the tests

---
+++ tests/test_album.py
@@ -0,0 +1,7 @@
+import unittest
+from src.logica.coleccion import Colleccion
+from src.modelo.album import Album, Medio
+from src.modelo.declarative_base import Session
+
+class AlbumTestCase(unittest.TestCase):
+    
